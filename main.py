b70407b1073b1596294ba45fa367378d0a5073d4
Şafak Aydın <91727090+saydin21@users.noreply.github.com>
2021-10-01 09:55:26 +0000

    I finished my first assignment!

---
+++ main.py
@@ -0,0 +1,15 @@
+import numpy
+print("Hello,there!")
+user_name= input("Who are you,dude? ")
+print("Great then, you can pass, ", user_name)
+
+x=int(input("enter number x:"))
+y=int(input("enter number y:"))
+print(x**y)
+print(numpy.log2(x))
+print("79164")
+
+
+
+
+
